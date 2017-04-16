5ed1b224a0549eee05f6ceb7be91bda3e2069948
Daniel Santa Maria <santamaria@utexas.edu>
2017-04-16 14:46:38 -0500

    added LFSR for pattern generation

--- project.v
+++ project.v
@@ -9,21 +9,41 @@ module bist_hardware(clk,rst,bistmode,bistdone,bistpass,cut_scanmode,
   output         cut_sdi;
   input          cut_sdo;
 
-  parameter      s_idle=0, s_test=1, s_compare=2, s_done=3, s_error=4;
+  parameter      s_idle=0, s_test=1, s_compare=2, s_done=3, s_error=4, s_capture=5, s_scan_in=6, s_scan_out=7;
+  parameter      shift_done=227;
   parameter      count_max=2000; //number of patterns for bist done
   parameter      ff_signature=16'hdead; //TODO: find correct fault free signature
+  parameter      initial_value=16'hbeef;
 // Add your code here
   reg [11:0] count;
+  reg [7:0]  shift_count;
   reg [3:0]  state;
   reg [3:0]  next_state;
   reg        cut_scanmode_reg, bistdone_reg, bistpass_reg;
-  reg [15:0] signature;
+  reg [15:0] signature, LFSR, MISR;
  
   assign cut_scanmode = cut_scanmode_reg;
   assign bistdone = bistdone_reg;
   assign bistpass = bistpass_reg;
+  assign cut_sdi = LFSR[15];
   //TODO: reset cut_scanmode_reg
-  //
+
+
+   //always @()
+  
+   //Pattern_generator
+   always @(posedge clk or posedge rst) begin
+     if (rst == 1) begin
+       LFSR <= initial_value;
+     end
+     else if(cut_scanmode_reg) begin
+       LFSR <= {LFSR[14:0], LFSR[15]^LFSR[4]^LFSR[3]^LFSR[2]^LFSR[0]};
+     end
+     else begin
+       LFSR <= initial_value;
+     end
+   end
+
    always @(posedge clk or posedge rst) begin
      if (rst == 1) begin
        state <= s_idle;
@@ -34,15 +54,15 @@ module bist_hardware(clk,rst,bistmode,bistdone,bistpass,cut_scanmode,
    end 
 
    //state machine
-   always @(posedge clk) begin 
+   always @(state) begin 
      case(state)
        s_idle: begin
          if (rst == 1 && bistmode == 1) begin
-           count <= 0;
+           count = 0;
            cut_scanmode_reg = 1;
-           next_state <= s_test;
-           bistdone_reg <= 0;
-           bistpass_reg <= 0;
+           next_state = s_test;
+           bistdone_reg = 0;
+           bistpass_reg = 0;
          end
          else begin
            next_state <= s_idle;
@@ -52,33 +72,43 @@ module bist_hardware(clk,rst,bistmode,bistdone,bistpass,cut_scanmode,
        s_test: begin
          if(count == count_max) begin
            cut_scanmode_reg = 0;
-           state <= s_compare;
+           state = s_compare;
+         end
+         else if(shift_count == shift_done) begin
+           state = s_capture;
          end
          else begin
-           count <= count + 1;
-           state <= s_test;
+           cut_scanmode_reg = 1;
+           shift_count = shift_count + 1;
+           state = s_test;
          end
        end
 
+       s_capture: begin
+         count = count + 1;
+         cut_scanmode_reg = 0;
+         state = s_test;
+       end
+ 
        s_compare: begin
          if(signature == ff_signature) begin
-           state <= s_done;
+           state = s_done;
          end
          else begin
-           state <= s_error;
+           state = s_error;
          end
        end
 
        s_done: begin
-         bistdone_reg <= 1;
-         bistpass_reg <= 1;
-         state <= s_idle;
+         bistdone_reg = 1;
+         bistpass_reg = 1;
+         state = s_idle;
        end
 
        s_error: begin
-         bistdone_reg <= 1;
-         bistpass_reg <= 0;
-         state <= s_idle;
+         bistdone_reg = 1;
+         bistpass_reg = 0;
+         state = s_idle;
        end
     
        default: $display("ERROR: Entered unknown state\n");
